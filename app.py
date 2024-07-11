5f60fdbefe9ccb003f98974f44a0df9825640865
Travis Frisinger <tmfrisinger@gmail.com>
2024-07-10 19:12:46 -0600

    new song tiny terror + header image for sound cloud

--- app.py
+++ app.py
@@ -21,9 +21,9 @@ def create_video(image_path, audio_path, output_path, fps=24):
     video_clip.write_videofile(output_path, codec='libx264', audio_codec='aac', fps=fps)
 
 # Paths to the image, audio, and output video file
-image_path = "images/Mr. Fluffles Fury.png"
-audio_path = "mp3/Mr. Fluffles Fury.mp3"
-output_path = "Mr. Fluffles Fury.mp4"
+image_path = "images/Mr. Fluffles' Reign of Tiny Terror.png"
+audio_path = "mp3/Mr. Fluffles' Reign of Tiny Terror.mp3"
+output_path = "Mr. Fluffles' Reign of Tiny Terror.mp4"
 
 # Create the video
 create_video(image_path, audio_path, output_path)
